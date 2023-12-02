4f7079babe9f9509206203c100a6f71a757c0596
min429 <lsm990429@gmail.com>
2023-12-02 17:28:07 +0900

    dbconfig, contents-now 수정

---
+++ src/php/dbconfig.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "43.201.98.27:52388";
+$username = "root";
+$password = "1234";
+$dbname = "FILM_INDUSTRY"; // 데이터베이스 이름
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if($conn->connect_error){
+	die("Connection failed: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
+?>
+
