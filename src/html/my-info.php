cd9d4c85adec518b80de462fa5a73d058a6cc5ae
hoihoi0101 <114476214+hoihoi0101@users.noreply.github.com>
2023-12-02 20:18:32 +0900

    로그인 헤더 업로드

---
+++ src/html/my-info.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../css/contents.css">
+    <script src="../js/main_video.js"></script>
+</head>
+<body>
+    <hr>
+    <div class="scrollBar" style="width: 1440px; height:800px; overflow-y: scroll;">
+        <div class="movies">
+            <?php        
+            include '../php/dbconfig.php';
+
+            // 데이터베이스에서 정보 가져오기
+            $sql = "SELECT MV_name, Grade, Audi_num FROM Movie LIMIT 10";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                // 결과를 행 단위로 출력
+                while($row = $result->fetch_assoc()) {
+                    echo '<a href="info.html?value=' . strtolower(str_replace(' ', '_', $row["MV_name"])) . '">';
+                    echo '<div class="movie-card">';
+                    echo '<div class="video-container">';
+                    // echo '<img src="../../img/poster/' . $row["MV_name"] . ' 포스터.jpg" alt="" class="movie-img-main">';
+                    echo '</div>';
+                    echo '<div class="movie-text">';
+                    echo '<div class="movie-name-main">' . $row["MV_name"] . '</div>';
+                    echo '<div class="movie-grade">★ ' . $row["Grade"] . '</div>';
+                    echo '<div class="audience-value">' . $row["Audi_num"] . '명</div>';
+                    // echo '<div class="movie-contents">' . $row["Contents"] . '</div>';
+                    echo '</div>';
+                    echo '</div>';
+                    echo '</a>';
+                }
+            } else {
+                echo "0 results";
+            }
+            ?>
+        </div>
+    </div>
+</body>
+</html>
