0aa0256ff50ac4d7160c6d3656ffce0123627b2b
hoihoi0101 <114476214+hoihoi0101@users.noreply.github.com>
2023-12-03 15:37:49 +0900

    내 정보

--- src/html/my-info.php
+++ src/html/my-info.php
@@ -21,6 +21,16 @@
         $sql = "SELECT USR_ID, USR_name, Phone_Num FROM User WHERE USR_ID = '$loggedInUserID'";
         $result = $conn->query($sql);
 
+        $sql2 = "SELECT U.USR_name, R.rating, R.content, M.MV_name
+         FROM Review R 
+         INNER JOIN User U ON R.USR_ID = U.USR_ID 
+         INNER JOIN Movie M ON R.MV_code = M.MV_code
+         WHERE R.USR_ID = '$loggedInUserID'";
+        $result2 = $conn->query($sql2);
+
+        //$sql3 = "SELECT USR_ID, USR_name, Phone_Num FROM User WHERE USR_ID = '$loggedInUserID'";
+        //$result3 = $conn->query($sql3);
+
         if ($result->num_rows > 0) {
             // 결과를 가져와서 표시
             while($row = $result->fetch_assoc()) {
@@ -36,13 +46,29 @@
                 echo '</div>';
                 echo '</div>';
 
-                // 리뷰 컨테이너
-                echo '<div class="container">';
-                echo '<div class="movie-info">';
-                echo '<div class="movie-info-content">';
-                echo '<h2>리뷰</h2>';
-                echo '</div>';
-                echo '</div>';
+                if ($result2->num_rows > 0) {
+                    echo '<div class="review-section">';
+                    echo '<h2>리뷰</h2>';
+                    echo '<ul class="review-list">';
+                    while ($row2 = $result2->fetch_assoc()) {
+                        $mvname = $row2['MV_name'];
+                        $username = $row2['USR_name'];
+                        $rating = $row2['rating'];
+                        $content = $row2['content'];
+
+                        echo '<li class="review-item">';
+                        echo '<div class="name">' . $mvname . '</div>';
+                        echo '<div class="name">' . $username . '</div>';
+                        echo '<div class="ratingStars" id="rating_' . $username . '"></div>';
+                        echo '<div class="content">' . $content . '</div>';
+                        echo '</li><br>';
+                        echo "<script>displayRating($rating, 'rating_$username');</script>";
+                    }
+                    echo '</ul>';
+                    echo '</div>';
+                } else {
+                    echo '리뷰가 없습니다.';
+                }
 
                 //찜 컴테이너
                 echo '<div class="container">';
