eb07cdf4abceb3a5f9f890fd587da83cad869911
hoihoi0101 <114476214+hoihoi0101@users.noreply.github.com>
2023-12-09 11:34:02 +0900

    리뷰 로직 수정

--- src/html/review-register.php
+++ src/html/review-register.php
@@ -3,15 +3,27 @@ include '../php/dbconfig.php';
 session_start();
 // 여기서 로그인한 사용자의 ID를 가져오는 방법이 있어야 합니다.
 $loggedInUserID = $_SESSION['loggedin_user_id'];
-$movieCode =   $_SESSION['mv_code'];
+$movieCode = $_SESSION['mv_code'];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $rating = $_POST['rating'];
     $content = $_POST['content'];
 
-    // 사용자 정보 및 리뷰를 Review 테이블에 삽입하는 쿼리
-    $insertReview = "INSERT INTO Review (USR_ID, MV_code, rating, content) VALUES ('$loggedInUserID', '$movieCode', '$rating', '$content')";
+    // RV_code 값 가져오기
+    $getRVCodeQuery = "SELECT MAX(RV_code) AS max_rv_code FROM Review WHERE USR_ID = '$loggedInUserID' AND MV_code = '$movieCode'";
+    $result = $conn->query($getRVCodeQuery);
+    $row = $result->fetch_assoc();
+    $maxRVCode = $row['max_rv_code'];
+    
+    // RV_code 값 설정
+    if ($maxRVCode === null) {
+        $newRVCode = 1;
+    } else {
+        $newRVCode = $maxRVCode + 1;
+    }
 
+    // 사용자 정보 및 리뷰를 Review 테이블에 삽입하는 쿼리
+    $insertReview = "INSERT INTO Review (USR_ID, MV_code, RV_code, rating, content) VALUES ('$loggedInUserID', '$movieCode', '$newRVCode', '$rating', '$content')";
 
     if ($conn->query($insertReview) === TRUE) {
         header("Location: info.php?value=" . $movieCode);
